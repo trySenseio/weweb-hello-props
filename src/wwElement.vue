f68e3f896b9e0d6792d80d1f22f3fd5ed6d142a9
Leon Müller <leon@Mac.fritz.box>
2025-04-13 19:38:43 +0200

    Initial commit – Hello World Component

---
+++ src/wwElement.vue
@@ -0,0 +1,24 @@
+<template>
+    <div :style="{ color: content.textColor }">
+      {{ content.message }}
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    props: {
+      content: {
+        type: Object,
+        required: true,
+      },
+    },
+  }
+  </script>
+  
+  <style scoped>
+  div {
+    font-size: 24px;
+    font-weight: bold;
+  }
+  </style>
+  
---
+++ ww-config.js
@@ -0,0 +1,19 @@
+export default {
+    editor: {
+      label: 'Hello World Element',
+      icon: 'text_fields',
+    },
+    properties: {
+      message: {
+        label: 'Text',
+        type: 'text',
+        defaultValue: 'Hello from WeWeb 👋',
+      },
+      textColor: {
+        label: 'Textfarbe',
+        type: 'color',
+        defaultValue: '#333333',
+      },
+    },
+  }
+  
