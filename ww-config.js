aea12ad3e7b0fdaff66c058de9e2f262c636e2fb
Leon Müller <leon@bc98e757-6cae-4a5b-b994-afa43de80962.fritz.box>
2025-04-14 21:58:41 +0200

    Interaktive Komponente (Counter) erweitert um diverse Funktionen

--- ww-config.js
+++ ww-config.js
@@ -1,8 +1,29 @@
 export default {
     editor: {
-      label: 'Interaktiver Counter',
+      label: 'Counter mit Logik',
       icon: 'plus_one',
     },
-    properties: {},
+    properties: {
+      start: {
+        label: 'Startwert',
+        type: 'number',
+        defaultValue: 0,
+        bindable: true,
+        editable: true,
+      },
+      step: {
+        label: 'Schrittgröße',
+        type: 'number',
+        defaultValue: 1,
+        bindable: true,
+        editable: true,
+      },
+      value: {
+        label: 'Zählerwert',
+        type: 'number',
+        bindable: true,
+        readonly: true, // damit du es z. B. im Workflow nutzen kannst
+      },
+    },
   }
   
